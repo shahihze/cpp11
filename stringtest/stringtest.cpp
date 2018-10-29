9df665ef80a87a0fbb8d5cc56d2c774959797875
shahihze <39230138+shahihze@users.noreply.github.com>
2018-10-29 20:45:27 +0800

    add comment 1029

--- stringtest/stringtest.cpp
+++ stringtest/stringtest.cpp
@@ -5,7 +5,7 @@ string path = "C:\Program Files (x86)\alipay\aliedit\5.1.0.3754";
 string path2 = "C:\\Program Files (x86)\\alipay\\aliedit\\5.1.0.3754";
 string path3 = R"(C:\Program Files (x86)\alipay\aliedit\5.1.0.3754)";
 string path4 = R"(C:\Program "Files" (x86)\\alipay\aliedit\5.1.0.3754)";
-
+//C++11原生字符串，不需要转义
 int main(int argc, char *argv[])
 {
     cout<<path<<endl;
